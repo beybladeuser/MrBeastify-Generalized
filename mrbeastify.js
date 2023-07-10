1109aab58b20a14c793661450bdf08135c80e6ca
beybladeuser <rubencruz0312@gmail.com>
2023-07-10 23:56:19 +0100

    Added implemented MrBeastifyOptions & ChaosLvl

--- mrbeastify.js
+++ mrbeastify.js
@@ -1,8 +1,12 @@
 const imagesPath = "images/";
 const images = [];
 
+let oldSettings = {};
+
+let overlayImagesQueue = []
+
 // Apply the overlay
-function applyOverlay(thumbnailElement, overlayImageUrl, flip) {
+function applyOverlay(thumbnailElement, overlayImageUrl, flip, settings) {
 	// Create a new img element for the overlay
 	const overlayImage = document.createElement("img");
 
@@ -14,7 +18,7 @@ function applyOverlay(thumbnailElement, overlayImageUrl, flip) {
 	overlayImage.style.width = "100%";
 	overlayImage.style.height = "100%";
 
-	if (location.href.includes("twitch")) {
+	if (location.href.includes("twitch") && thumbnailElement.previousElementSibling !== null) {
 		thumbnailElement.previousElementSibling.style.position = "absolute";
 	}
 
@@ -28,47 +32,67 @@ function applyOverlay(thumbnailElement, overlayImageUrl, flip) {
 	thumbnailElement.style.position = "relative";
 	// Append the overlay to the parent of the thumbnail
 	thumbnailElement.parentElement.appendChild(overlayImage);
+
+	return overlayImage;
 }
 
-function getThumbnailElements() {
-	const url = location.href;
-
+function getElementQuery(settings) {
 	let elementQuery = null;
 	if (url.includes("youtube")) {
 		elementQuery =
 			"ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element)";
 	} else if (url.includes("twitch")) {
-		//elementQuery = "article > div.tw-hover-accent-effect > div > a > div > div.tw-aspect > img:nth-child(1)";
-		elementQuery = "article > div.tw-hover-accent-effect > div > a > div > div.tw-aspect > img:only-of-type";
+		elementQuery = settings.ChaosLvl !== "0" ? "article > div.tw-hover-accent-effect > div > a > div > div.tw-aspect > img:nth-child(2)" : "article > div.tw-hover-accent-effect > div > a > div > div.tw-aspect > img:only-of-type";
 	} else if (url.includes("pornhub")) {
-		elementQuery = ".js-videoThumb:not(.js-menuSwap):only-of-type";
+		elementQuery = settings.ChaosLvl !== "0" ? ".js-videoThumb:not(.js-menuSwap)" : ".js-videoThumb:not(.js-menuSwap):only-of-type";
 	}
+	return elementQuery;
+}
 
-	if (elementQuery !== null) {
+function getThumbnailElements(settings) {
+	const url = location.href;
+
+	
+
+	if (elementQuery !== null && settings.MrBeastifyOptions !== "1") {
 		return document.querySelectorAll(elementQuery);
-	} else if (document.querySelectorAll("#MrBeast").length === 0) {
+	} else if (settings.MrBeastifyOptions === "1" && settings.ChaosLvl === "0") {
+		return Array.from(document.querySelectorAll("img")).filter((e) => {return e.parentElement.querySelectorAll("#MrBeast").length === 0});
+	} else if (settings.MrBeastifyOptions === "1" && settings.ChaosLvl === "1") {
+		return document.querySelectorAll("img:not(#MrBeast)");
+	} else if (settings.MrBeastifyOptions === "1" && settings.ChaosLvl === "2") {
 		return document.querySelectorAll("img");
 	}
+}
+
+function removeMrBeasts() {
+	document.querySelectorAll("#MrBeast")?.forEach((e, i) => {
+		e.remove();
+	})
+	overlayImagesQueue = []
 }
 
 // Looks for all thumbnails and applies overlay
 function applyOverlayToThumbnails() {
 	//Asks background for settings 
-	chrome.runtime.sendMessage({ action: "getSettings" }, function (settings) {
+	chrome.runtime.sendMessage({ action: "getSettings" }, function (newSettings) {
 		//Removes all MrBeasts and exits early
-		if (!settings.isOnSwitch) {
-			document.querySelectorAll("#MrBeast").forEach((e, i) => {
-				e.remove();
-			})
+		if (!newSettings.isOnSwitch) {
+			removeMrBeasts()
 			return;
 		}
+		if (oldSettings.MrBeastifyOptions !== newSettings.MrBeastifyOptions) {
+			removeMrBeasts()
+		}
+		oldSettings = newSettings
 
 		// Query all YouTube video thumbnails on the page that haven't been processed yet
 		// (ignores shorts thumbnails)
-		const thumbnailElements = getThumbnailElements();
+		const thumbnailElements = getThumbnailElements(newSettings);
 
+		const newOverlayImages = []
 		// Apply overlay to each thumbnail
-		thumbnailElements.forEach((thumbnailElement) => {
+		thumbnailElements?.forEach((thumbnailElement) => {
 			// Apply overlay and add to processed thumbnails
 			let loops = Math.random() > 0.001 ? 1 : 20; // Easter egg
 
@@ -76,9 +100,18 @@ function applyOverlayToThumbnails() {
 				// Get overlay image URL from your directory
 				const overlayImageUrl = getRandomImageFromDirectory();
 				const flip = Math.random() < 0.25; // 25% chance to flip the image
-				applyOverlay(thumbnailElement, overlayImageUrl, flip);
+				newOverlayImages.push(applyOverlay(thumbnailElement, overlayImageUrl, flip, newSettings));
 			}
 		});
+		if (newSettings.ChaosLvl === "1") {
+			overlayImagesQueue.push(newOverlayImages);
+			for (let i = newSettings.MaxOverlayQueue; i <= overlayImagesQueue.length; i++) {
+				overlayImagesToDelete = overlayImagesQueue.shift()
+				overlayImagesToDelete.forEach((e, i) => {
+					e.remove()
+				})
+			}
+		}
 	});
 
 
